d43b65d196a16113c9fed2e0b75396bbef5fa328
Anthony Rivas <kamikazebot@gmail.com>
2018-03-22 22:50:39 -0700

    Adding fb button

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).load(function () {
+	FB.getLoginStatus(function (response) {
+		statusChangeCallback(response);
+	});
+
+
+
+
+	function checkLoginState() {
+		FB.getLoginStatus(function (response) {
+			statusChangeCallback(response);
+		});
+	}
+})
