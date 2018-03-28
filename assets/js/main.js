4383c3d5350bc183dd11dc1e4bdafa91c566861c
Anthony Rivas <AnthonyR@ccs.local>
2018-03-28 13:45:08 -0700

    Login test

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+function statusChangeCallback( response ) {
+	console.log( 'statusChangeCallback' );
+	console.log( response );
+	if ( response.status === 'connected' ) {
+		testAPI();
+
+	} else if ( response.status === 'not_authorized' ) {
+		FB.login( function ( response ) {
+			statusChangeCallback2( response );
+		}, { scope: 'public_profile,email' } );
+
+	} else {
+		alert( "not connected, not logged into facebook, we don't know" );
+	}
+}
+
+function statusChangeCallback2( response ) {
+	console.log( 'statusChangeCallback2' );
+	console.log( response );
+	if ( response.status === 'connected' ) {
+		testAPI();
+
+	} else if ( response.status === 'not_authorized' ) {
+		console.log( 'still not authorized!' );
+
+	} else {
+		alert( "not connected, not logged into facebook, we don't know" );
+	}
+}
+
+function checkLoginState() {
+	FB.getLoginStatus( function ( response ) {
+		statusChangeCallback( response );
+	} );
+}
+
+function testAPI() {
+	console.log( 'Welcome!  Fetching your information.... ' );
+	FB.api( '/me', function ( response ) {
+		console.log( 'Successful login for: ' + response.name );
+		document.getElementById( 'status' ).innerHTML =
+			'Thanks for logging in, ' + response.name + '!';
+	} );
+}
+
+$( document ).ready( function () {
+	FB.init( {
+		appId: '178543329602371',
+		xfbml: true,
+		version: 'v2.2'
+	} );
+	checkLoginState();
+} );
